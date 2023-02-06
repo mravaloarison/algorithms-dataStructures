f55d1a12743cb4f7e0d0d1fb636ca8a1e8468838
mami-em <maminiainaravaloarison@gmail.com>
2023-02-06 01:23:06 -0500

    comments added for phone

--- 04_phone.js
+++ 04_phone.js
@@ -1,14 +1,16 @@
 function telephoneCheck(str) {
+    /* 
+        The program would look for the pattern first, 
+            - if the number inserted looks like a phone pattern, 
+                - then it will check if it starts with one or not,
+                    - if yes and the length is 11 then return true, if the length is more or less it will return false
+                    - else it will just look at the length if it is 10, then return true if so and return false otherwise
+
+            - else, it will return false instantly
+    */
     let phone_number = str.split(/\D/).join("");
-    let pattern = [];
-  
-    for (let i in str) {
-      !parseInt(str[i]) ?
-      pattern.push(str[i]) :
-      pattern.push('*')
-    }
-  
-    let patterns = [
+    let pattern      = [];
+    let patterns     = [
       '* (***) *** ****',
       '* (***) ***-****',
       '*(***)***-****',
@@ -22,6 +24,15 @@ function telephoneCheck(str) {
       '**********'
     ]
   
+    // insert into an array the format of the str inserted
+    for (let i in str) {
+      !parseInt(str[i]) ?
+      pattern.push(str[i]) :
+      pattern.push('*')
+    }
+  
+  
+    // check if the pattern match to a phone number's patern
     let pattern_met = 0
     patterns.map(item => {
       if (item === pattern.join("")) {
@@ -29,6 +40,7 @@ function telephoneCheck(str) {
       };
     })
   
+    // conditions if the patterns match
     if (pattern_met > 0) {
       if (phone_number.length == 11 && phone_number[0] == 1) {
         return true;
