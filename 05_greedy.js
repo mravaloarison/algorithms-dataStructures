32b6d17cb8a36d51b7da3653f375b6d24dbbe8f5
mami-em <maminiainaravaloarison@gmail.com>
2023-02-05 22:50:02 -0500

    break while loop changes

--- 05_greedy.js
+++ 05_greedy.js
@@ -1,26 +1,30 @@
 let arrOfChange = [50, 20, 10, 5, 2 , 1, .50, .25, .10, .05, .01];
 
-let amount_paid = 98.36;
+let amount_paid = 98.38;
 
 let changes = [];
 let count = 0;
 
 let i = 0;
 while (amount_paid >= 0) {
-  if (amount_paid == 0){
-    break;
-  }
+    // stop when there is no changes no more
+    if (amount_paid == 0){
+        changes.push({count: count, change: arrOfChange[i]});
+        break;
+    }
 
-  else if (arrOfChange[i] <= amount_paid) {
-    amount_paid = (amount_paid - arrOfChange[i]).toFixed(2);
-    count++;
-  } 
-  
-  else {
-    changes.push({count: count, change: arrOfChange[i]});
-    count = 0;
-    i++;
-  }
+    // check if changes can still be given
+    else if (arrOfChange[i] <= amount_paid) {
+        amount_paid = (amount_paid - arrOfChange[i]).toFixed(2);
+        count++;
+    } 
+    
+    // udpate the changes
+    else {
+        changes.push({count: count, change: arrOfChange[i]});
+        count = 0;
+        i++;
+    }
 }
 
-console.log(changes.filter(item => item.count != 0))
+console.log(changes.filter(item => item.count != 0));
