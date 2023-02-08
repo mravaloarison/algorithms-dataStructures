bb00d04ee3b97f2f84bb3ab079669f2811faa116
mami-em <maminiainaravaloarison@gmail.com>
2023-02-07 21:31:47 -0500

    cash success

--- 06_cash.js
+++ 06_cash.js
@@ -1,48 +1,71 @@
 function checkCashRegister(price, cash, cid) {
-  let changeDue = cash - price;
-  let idealCid = [];
-  let availableChange = cid.reduce((a,item) => a + item[1], 0);
-  let arrOfChange = [["ONE HUNDRED",100],["TWENTY",20], ["TEN",10], ["FIVE",5] , ["ONE",1], ["QUARTER",.25], ["DIME",.10], ["NICKEL",.05], ["PENNY",.01]];
+  const arrOfChange = [["ONE HUNDRED",100],["TWENTY",20], ["TEN",10], ["FIVE",5] , ["ONE",1], ["QUARTER",.25], ["DIME",.10], ["NICKEL",.05], ["PENNY",.01]];
+  let change = cash - price;
+  let newCid = [];
+  let availableCid = [];
 
+  // look at only the funds that can be usedx
+  for (let i in arrOfChange) {
+    if (change >= arrOfChange[i][1] && cid[cid.length - 1 - i][1] >= arrOfChange[i][1]) {
+      cid
+      .filter(item => item[0] == arrOfChange[i][0])
+      .map(item => availableCid.push([item[0],item[1]]));
+    }
+  }
 
-  if (availableChange.toFixed(2) < changeDue) {
+  let funds = availableCid
+  .map(item => item[1])
+  .reduce((a,b) => a + b, 0);
+
+  // if the available funds is less than the change due
+  if (funds.toFixed(2) < change) {
     return {status: "INSUFFICIENT_FUNDS", change: []}
   }
 
-  decompose(changeDue, arrOfChange)
-  .map(item => {
-    idealCid.push([item.curencyUnit[0], item.amount * item.curencyUnit[1] ])
-  });
+  // if the available funds is equal to the change due
+  else if (funds.toFixed(2) == change) {
+    return {status: "CLOSED", change: cid};
+  }
 
-  return {status: "OPEN", change: idealCid};
-}
+  while (change > 0) {
+    
+    for (let i in availableCid) {
 
+      // total change paid
+      if (availableCid[i][1] == change) {
+        newCid.push([availableCid[i][0], change])
+        change = 0;
+        break;
+      }
 
-function decompose(change, arrOfChange) {
-  let changeDue = change.toFixed(2);
-  let changes   = [];
-  let count     = 0;
+      // there are still some rest of the change
+      else if (availableCid[i][1] < change) {
+        newCid.push([availableCid[i][0], availableCid[i][1]])
+        change = (change - availableCid[i][1]).toFixed(2);
+      }
 
-  let i = 0;
-  while (changeDue >= 0) {
-    if (changeDue == 0) {
-      changes.push({amount: count,curencyUnit: arrOfChange[i]});
-      break;
-    }
+      else {
+        let value = arrOfChange
+        .filter(item => item[0] == availableCid[i][0])
+        .map(item => item[1])
+        .reduce((a,b) => a + b);
+        
+        let countAv = 1;
+        while (true) {
+          if ((countAv * value) > change) {
+            countAv--;
+            break;
+          }
+          countAv++;
+        }
 
-    if (changeDue >= arrOfChange[i][1]) {
-      changeDue = (changeDue - arrOfChange[i][1]).toFixed(2);
-      count ++;
-    }
-
-    else {
-      changes.push({amount: count,curencyUnit: arrOfChange[i]});
-      count = 0;
-      i++;
+        newCid.push([availableCid[i][0], value * countAv])
+        change = (change - (value * countAv)).toFixed(2);
+      }
     }
   }
 
-  return changes.filter(item => item.amount > 0);
+  return {status: "OPEN", change: newCid.filter(item => item[1] != 0)};
 }
 
 console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
